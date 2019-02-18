4e651fe35f79fcec7e0d2e455c2458a1e41da509
Tom <tom.terado@gmail.com>
2019-02-19 00:21:51 +0900

    Finished

--- palindromes.js
+++ palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function() {
+const palindromes = function(string) {
+    var newString = string.toLowerCase().replace(/[^A-Za-z]/g, "");
+    return (newString.split("").reverse().join("") == newString);
+//     if (newString === string){
+//         return true;
+//     } else {
+//         return false;
+//     }
 
 }
+
+// const palindromes = function(string) {
+//     processedString = string.toLowerCase().replace(/[^A-Za-z]/g, "");
+//     return (
+//       processedString
+//         .split("")
+//         .reverse()
+//         .join("") == processedString
+//     );
+//   };
 
 module.exports = palindromes
